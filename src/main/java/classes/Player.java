630515e81a150fd31bdf79cfe6c36bdc1839cf2f
German Aguirre <gaguirre@atlassian.com>
2017-12-17 21:35:11 -0300

    Initial commit

---
+++ src/main/java/classes/Player.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Player {
+
+    private String name;
+    private String tournamentLevel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
