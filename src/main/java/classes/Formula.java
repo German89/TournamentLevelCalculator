3d2a77db7c160349f357feaab0fc6ea4b5ae090a
German Aguirre <german.aguirre@atginfo.com>
2017-12-21 15:59:40 -0300

    Implemented the method to use the right formula depending on which played won

--- src/main/java/classes/Formula.java
+++ src/main/java/classes/Formula.java
@@ -2,17 +2,80 @@ package classes;
 
 public class Formula {
 
-   /* Integer getTorunamentLevelAfterMach(Player player, Player opponent, String winningPlayer){
-
+   Integer getTorunamentLevelAfterMach(Player player, Player opponent, String winningPlayer){
+        int tournamentlevelEarnedLost = 0;
 
         if(winningPlayer.equals(player.getName())){
-            if(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament() >0){
-
+            //If the Player won the match it will earn points depending on the formula applied
+            if(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament() > 0){
+                tournamentlevelEarnedLost = getPointsWinningPlayerHasMoreLevel(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament());
+            }else {
+                tournamentlevelEarnedLost = getPointsWinningPlayerHasLessLevel(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament());
             }
-        }else {
 
+        }else {
+            //If the player lost the match it will lost points depending on the formula applied
+            if(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament() > 0){
+                tournamentlevelEarnedLost = getPointsWinningPlayerHasMoreLevel(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament());
+            }else {
+                tournamentlevelEarnedLost = getPointsWinningPlayerHasLessLevel(player.getTournamentLevelBeforeTournament() - opponent.getTournamentLevelBeforeTournament());
+            }
+            tournamentlevelEarnedLost = - tournamentlevelEarnedLost;
         }
 
-    }*/
+        return tournamentlevelEarnedLost;
+
+    }
+
+    public int getPointsWinningPlayerHasMoreLevel(int differencePlayerLevel){
+       //Formula to apply if the winning player has more level than the looser
+       if(differencePlayerLevel < 25){
+           return 18;
+       }else if(differencePlayerLevel < 50){
+            return 16;
+        }else if(differencePlayerLevel < 100){
+           return 14;
+        }else if(differencePlayerLevel < 150){
+           return 12;
+       }else if(differencePlayerLevel < 200){
+        return 10;
+        }else if(differencePlayerLevel < 300){
+           return 8;
+       }else if(differencePlayerLevel < 400){
+           return 6;
+       }else if(differencePlayerLevel < 500){
+           return 4;
+       }else if(differencePlayerLevel < 1000){
+           return 4;
+       }else {
+           return 0;
+        }
+    }
+
+
+    public int getPointsWinningPlayerHasLessLevel(int differencePlayerLevel){
+        //Formula to apply if the winning player has less level than the looser
+        if(differencePlayerLevel < 25){
+            return 18;
+        }else if(differencePlayerLevel < 50){
+            return 22;
+        }else if(differencePlayerLevel < 100){
+            return 26;
+        }else if(differencePlayerLevel < 150){
+            return 30;
+        }else if(differencePlayerLevel < 200){
+            return 36;
+        }else if(differencePlayerLevel < 300){
+            return 42;
+        }else if(differencePlayerLevel < 400){
+            return 48;
+        }else if(differencePlayerLevel < 500){
+            return 56;
+        }else if(differencePlayerLevel < 1000){
+            return 64;
+        }else {
+            return 80;
+        }
+    }
 
 }
--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -3,6 +3,7 @@ package classes;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -18,7 +19,20 @@ public class Main {
         List<Match> listOfMatches = xlsxReaderWriter.getListOfMatches(xssfWorkbook); //get the list of matches
 
 
+        for(Player player1 : listOfPlayers){
+            // Find all the matches of that player in the tournament
+            List<Match> listOfPlayerMatches = new ArrayList<Match>();
+            for(Match match1 : listOfMatches){
+                if (match1.getPlayer().equals(player1.getName())){
+                    listOfPlayerMatches.add(match1);
+                }
+            }
 
+            //Get the total of points earned by the
+
+
+
+        }
 
 
 
